ee106fbaac0deabed66b9c5fd984159dcbb97ebf
youinsxxn <105770764+youinsxxn@users.noreply.github.com>
2022-05-31 18:31:01 +0900

    Delete document directory

--- document/js/position05.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.smile').on('click', function () {
-        $(this).toggleClass('active')
-    });
-
-    $('.top_banner button').on('click', function () {
-        $('.top_banner .cotainer').toggleClass('active');
-    })
-})
